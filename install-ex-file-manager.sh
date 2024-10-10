a4b5dacc5eb2992ef49bdc3d2cb611676e706b97
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-10-10 19:39:25 -0400

    Update install-ex-file-manager.sh

--- install-ex-file-manager.sh
+++ install-ex-file-manager.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+
+#installs tgpt
+curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
+
+
+
+#installs fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sudo apt update && sudo apt install -y curl sudo ranger stterm
+
+
+
+#installs simple terminal and ranger 
+sudo apt update && sudo apt install -y curl ranger stterm
 
 sudo cp ~/exfilemanager/exfilemanagercli /usr/local/bin/
 
